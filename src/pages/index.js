e96c4bb80b60492688dd6e003debd15812fb7921
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2023-11-21 13:22:08 -0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,199 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+const inter = Inter({ subsets: ['latin'] });
+
+const NotificationWindow = ({ message, onClose }) => (
+  <div className="notification-window  bg-red-200 p-2 md:w-2/3 rounded-md font-bold text-gray-700  pl-4 xl:pl-7 ">
+          <button onClick={onClose} className='hover:text-red-600 transition duration-100 ease-linear'>x</button>
+
+    <div className="notification-content pl- pb-4">
+      <span>{message}</span>
+    </div>
+  </div>
+);
+
+export default function Home() {
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [load, setLoad] = useState('none');
+  const [showNotification, setShowNotification] = useState(false);
+  const [isRegister, setIsRegister] = useState(false);
+
+  const router = useRouter();
+
+
+  const toggleForm = () => {
+    setIsRegister(!isRegister);
+    // Reset form fields when toggling
+    setEmail('');
+    setPassword('');
+  };
+
+  const handleSubmit = async () => {
+    // Your form submission logic here
+    if (isRegister) {
+      // Handle register form submission
+      console.log('Registering:', email, password);
+      toggleForm()
+      const newData = await fetch('https://helpme123-e9fba62d59d0.herokuapp.com/quit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      }).then((res) => res.json());
+
+      // If registration is successful, toggle back to the login form
+
+    } else {
+      // Handle login form submission
+      const newData = await fetch('https://helpme123-e9fba62d59d0.herokuapp.com/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      }).then((res) => res.text());
+      if (newData === '1') {
+        setTimeout(() => {
+          setLoad('flex')
+        }, 350); 
+        setTimeout(() => {
+          router.push('/logged');
+        }, 2000); // 1500 milliseconds (1.5 seconds) delay
+      } else {
+        setShowNotification(true);
+
+      }
+    }
+  };
+
+  
+
+  return (
+    <>
+     <div className='absolute bg-slate-100 bg-opacity-75 w-full h-full  z-50 p-0 rounded-2xl flex flex-col justify-center items-center'
+          style={{ display: load,  backgroundSize: 'cover', backgroundPosition: 'center' }}
+        >
+          <svg class="w-12 h-12 animate-spin text-indigo-400" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 4.75V6.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M17.1266 6.87347L16.0659 7.93413" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M19.25 12L17.75 12" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M17.1266 17.1265L16.0659 16.0659" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M12 17.75V19.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M7.9342 16.0659L6.87354 17.1265" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M6.25 12L4.75 12" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M7.9342 7.93413L6.87354 6.87347" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+          </svg>
+        </div>
+      <main
+        className={`h-screen flex min-h-screen flex-col items-center justify-between p-8 md:p-16 ${inter.className} `}
+        style={{ backgroundImage: 'url(/images/4877010.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}
+      >
+       
+        <div className="w-full md:w-11/12 h-full bg-transparent flex shadow-2xl rounded-2xl">
+
+          <div   className='text-black bg-white w-full lg:w-1/2 flex flex-col justify-center rounded-2xl lg:rounded-bl-2xl lg:rounded-tl-2xl' >
+            <div className='flex flex-col m-16 md:m-16 text-xs xl:m-32 xl:text-base gap-5 '>
+              <h1 className='font-light text-xl md:text-2xl mb-6' data-aos="zoom-in-right">Welcome back. Please, log-in into your account.</h1>
+              <form className='space-y-6 items-center justify-center'        
+>
+                <h2 className='font-thin text-2xl md:text-3xl text-left mb-4'>{isRegister ? 'Register' : 'Login'}</h2>
+
+                <div className='flex flex-col space-y-2'  data-aos="fade-left"  data-aos-duration="2000" >
+                  <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    placeholder='E-mail'
+                    type='email'
+                    className='bg-transparent border-transparent border-b-blue-100 focus:border-b-rose-300 hover:border-b-rose-300  border rounded p-2 text-gray-700 focus:outline-none transition duration-300 ease-linear'
+                  />
+                </div>
+                <div className='flex flex-col space-y-2' data-aos="fade-left" data-aos-duration="2000" >
+                  <input
+                  
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    placeholder="Password"
+                    type='password'
+                    className='bg-transparent border-transparent border-b-blue-100 focus:border-b-sky-300 hover:border-b-sky-300 border rounded p-2 text-gray-700 focus:outline-none transition duration-300 ease-linear'
+                  />
+                </div>
+                <div className='flex mr-12 w-full' data-aos="zoom-in" data-aos-duration="1800">
+                  <button
+                    type='button'
+                    
+                    onClick={handleSubmit}
+                    style={{
+                      background: "linear-gradient(to left, #FB8994, #D04A9F)", // Adjust colors as needed
+                    }}
+                    class="border hover:scale-105 duration-300 relative group cursor-pointer text-white focus:scale-95 shadow-lg  overflow-hidden h-16 w-64 rounded-md  p-2 flex justify-center items-center font-extrabold"
+                  >
+                    <div
+                      className="absolute right-32 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-40 h-40 rounded-full group-hover:scale-150 duration-500"
+                      style={{
+                        background: "linear-gradient(to left, #FB8994, #D04A9F)", // Adjust colors as needed
+                      }}
+                    ></div>
+                    <div class="absolute right-2 -top-4  group-hover:top-1 group-hover:right-2 z-10 w-32 h-32 rounded-full group-hover:scale-150  duration-500 bg-sky-800" style={{
+                      background: "linear-gradient(to left, #A5EBED, #3D94C2)", // Adjust colors as needed
+                    }} ></div>
+                    <div class="absolute -right-12 top-4 group-hover:top-1 group-hover:right-2 z-10 w-24 h-24 rounded-full group-hover:scale-150  duration-500 bg-sky-700" style={{
+                      background: "linear-gradient(to left, #A5EBED, #3D94C2)", // Adjust colors as needed
+                    }}></div>
+                    <div class="absolute right-20 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-16 h-16 rounded-full group-hover:scale-150  duration-500 bg-sky-600" style={{
+                      background: "linear-gradient(to left, #F8D4B4, #FB9A92)", // Adjust colors as needed
+                    }}></div>
+                    <p class="z-10">{isRegister ? 'Register' : 'Enter'}</p>
+
+                  </button>
+                  
+
+
+                </div>
+                {showNotification && (
+          <NotificationWindow
+            message="Invalid email or password"
+            onClose={() => setShowNotification(false)}
+          />
+        )}
+              </form>
+              <span
+                onClick={toggleForm}
+                className={`w-3/4 mt-8 cursor-pointer border-b-0  transition-colors duration-300 ${isRegister ? 'text-gray-500 hover:text-sky-500 border-b-2 border-b-transparent hover:border-b-sky-300 w-1/2 ' : 'text-gray-500 hover:text-pink-500 border-b-2 border-b-transparent hover:border-b-pink-300'}`}
+              >
+                {isRegister ? 'Already has an account? Log-in' : "Doesn't have an account? Register"}
+
+              </span>
+
+            </div>
+          </div>
+          <div
+            className='rounded-br-2xl rounded-tr-2xl bg-slate-400 w-1/2 bg-gradient-radial from-green-300 to-blue-200 hidden lg:block'
+            style={{
+              backgroundImage: 'url(/images/b.jfif)',
+              backgroundSize: 'cover',
+              
+            }}
+            data-aos="flip-left"
+            data-aos-duration="2000"
+          >        {/* Content for the right column */}
+          </div>
+        </div>
+       
+      </main>
+    </>
+
+  );
+}
---
+++ src/pages/logged.js
@@ -0,0 +1,23 @@
+export default function Logged() {
+    return (
+        <div className="m-0 min-h-screen flex flex-col text-gray-600 font-light text-2xl justify-center items-center p-8" style={{ backgroundImage: 'url(/images/4877010.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <div className="mt-10 mb-10 flex flex-col justify-center items-center " >
+                <h1 className="text-7xl text-center md:text-8xl h-56 md:h-32 md:mb-4 bg-gradient-to-r from-blue-600 via-rose-400 to-pink-600 text-transparent bg-clip-text" data-aos="fade-right" data-aos-duration="2000" style={{ background: 'linear-gradient(to right, #3490dc, #fuchsia)' }}>You're logged in!</h1>
+                <h2 className="text-2xl  md:text-3xl mb-2 font-bold" data-aos="fade-right" data-aos-duration="2800">What does this mean?</h2>
+            </div>
+            <div className="w-5/6  lg:w-1/2 bg-white rounded-xl p-4 md:p-20 mt-10 mb-10 text-center text-gray-500 " data-aos="flip-right">
+                <p className="mb-4">
+                    Congratulations! You've successfully logged into your account. The information you provided on the login page has been sent to the SQL Server, verified, and returned!
+                </p>
+                <p className="mb-4">
+                    Behind the scenes, a series of processes occur. Your login information is queried against the data stored on our server. If your email and password match a user ID that was generated upon registering, you're redirected to this page!
+                </p>
+                <p className="mb-4">
+                    This "Login screen" project was created with the primary goal of practicing Next.js integration with Tailwind CSS, connecting to SQL databases, and deploying databases online using services such as Heroku Cloud. 
+                </p>
+                <p className="font-bold mb-4 "> Thank you for your time!</p>
+                <p className="font-light mb-1">See other projects of mine in my</p> <a href="https://pedro-portfolio-cyan.vercel.app" className="font-semibold hover:text-blue-400 transition duration-300 ease-linear ">Portfolio!</a>
+            </div>
+        </div>
+    );
+}
